32305b51c0a91230b087c4712ad35767be84e06d
Emma Hofer <109008584+hoferemma@users.noreply.github.com>
2023-11-26 18:19:26 -0600

    Update client.py

--- client.py
+++ client.py
@@ -1,35 +1,39 @@
+
 #clientlemurchat.py
 
 import socket
 import ssl
+import random
+
 
 def main():
     host = 'localhost'
     port = 1080
 
-    #creates client side socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # this is where the encrytion occurs, in the secure_socket
+    # Wrap the client socket with SSL for encryption
     context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
+
     secure_socket = context.wrap_socket(client_socket, server_hostname=host)
 
-    # connects using the secure socket
     secure_socket.connect((host, port))
 
-    # displays client list
+    # Receive and print the list of connected clients
     client_list = secure_socket.recv(1024).decode('utf-8')
     print(client_list)
 
     while True:
-        # continue typing or disconnect
+        # Prompt the user to enter a message or type 'exit' to disconnect
         message = input("Enter message in format 'target_id:message' or 'exit' to disconnect: ")
         if message == 'exit':
-            # option to disconnect
+            # Send the "exit" message to the server to disconnect
             secure_socket.send(message.encode('utf-8'))
             break
         else:
-            # continue sending messages
+            # Send a message using the target client's unique identifier
             secure_socket.send(message.encode('utf-8'))
 
     secure_socket.close()
