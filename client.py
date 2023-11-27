6d8b0bd0c95c712641f4c8e0f74dc2ecc8713d79
widowvision <68030945+widowvision@users.noreply.github.com>
2023-11-26 18:06:02 -0600

    Update client.py

--- client.py
+++ client.py
@@ -7,27 +7,29 @@ def main():
     host = 'localhost'
     port = 1080
 
+    #creates client side socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Wrap the client socket with SSL for encryption
+    # this is where the encrytion occurs, in the secure_socket
     context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
     secure_socket = context.wrap_socket(client_socket, server_hostname=host)
 
+    # connects using the secure socket
     secure_socket.connect((host, port))
 
-    # Receive and print the list of connected clients
+    # displays client list
     client_list = secure_socket.recv(1024).decode('utf-8')
     print(client_list)
 
     while True:
-        # Prompt the user to enter a message or type 'exit' to disconnect
+        # continue typing or disconnect
         message = input("Enter message in format 'target_id:message' or 'exit' to disconnect: ")
         if message == 'exit':
-            # Send the "exit" message to the server to disconnect
+            # option to disconnect
             secure_socket.send(message.encode('utf-8'))
             break
         else:
-            # Send a message using the target client's unique identifier
+            # continue sending messages
             secure_socket.send(message.encode('utf-8'))
 
     secure_socket.close()
